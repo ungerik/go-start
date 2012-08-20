8de1974c07a87389a3f231aa3eb3bff949a09f44
Erik Unger <erik@erikunger.com>
2012-08-20 17:32:37 +0200

    doc update

--- examples/static/main.go
+++ examples/static/main.go
@@ -1,9 +1,9 @@
 /*
 Download, build and run example:
 
-go get github.com/ungerik/go-start/examples/static
-cd github.com/ungerik/go-start/examples/static
-go install && static
+	go get github.com/ungerik/go-start/examples/static
+	cd github.com/ungerik/go-start/examples/static
+	go install && static
 
 */
 package main
