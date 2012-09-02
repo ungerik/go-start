7f817c641b101aa259dbbbba061cc5597ff41607
Erik Unger <erik@erikunger.com>
2012-09-02 08:57:14 +0200

    added media.ImageDataURL() and fixed media.ColoredImageDataURL()

--- media/functions.go
+++ media/functions.go
@@ -3,10 +3,15 @@ package media
 import (
 	"bytes"
 	"encoding/base64"
+	"fmt"
 	"image"
 	"image/color"
 	"image/draw"
 	"image/png"
+	"io/ioutil"
+	"net/http"
+	"path"
+	"strings"
 	"unicode"
 	"unicode/utf8"
 
@@ -39,10 +44,36 @@ func MakeValidUrlFilename(filename string) string {
 func ColoredImageDataURL(c color.Color) string {
 	i := image.NewRGBA(image.Rect(0, 0, 1, 1))
 	draw.Draw(i, i.Bounds(), image.NewUniform(c), image.ZP, draw.Src)
-	buf := bytes.NewBufferString("data:image/png;base64,")
-	err := png.Encode(buf, i)
+	var buf bytes.Buffer
+	err := png.Encode(&buf, i)
 	if err != nil {
 		panic(err)
 	}
-	return base64.URLEncoding.EncodeToString(buf.Bytes())
+	return "data:image/png;base64," + base64.URLEncoding.EncodeToString(buf.Bytes())
+}
+
+func ImageDataURL(imageURL string) (dataURL string, err error) {
+	var prefix string
+	switch strings.ToLower(path.Ext(imageURL)) {
+	case ".png":
+		prefix = "data:image/png;base64,"
+	case ".jpg", ".jpeg":
+		prefix = "data:image/jpeg;base64,"
+	case ".gif":
+		prefix = "data:image/gif;base64,"
+	default:
+		return "", fmt.Errorf("Invalid image filename extension in URL: %s", imageURL)
+	}
+
+	r, err := http.Get(imageURL)
+	if err != nil {
+		return "", err
+	}
+	defer r.Body.Close()
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return prefix + base64.URLEncoding.EncodeToString(data), nil
 }
