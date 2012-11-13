048cd19276f4bdf790905cab271d4ebe85d8d095
Erik Unger <erik@erikunger.com>
2012-11-13 10:24:19 +0100

    doc update

--- view/doc.go
+++ view/doc.go
@@ -1,4 +1,99 @@
 /*
 The View of MVC.
+
+Instead of using text templates, go-start creates HTML by using strongly typed Go composite literals.
+For some HTML constructs there are high level Go representations,
+but most HTML elements have direct Go type or shortcut function counterpart.
+
+Shortcut functions for HTML elements are all upper case,
+other utility functions follow the normal camel case naming convention.
+
+All shortcuts (see shortcuts.go):
+
+	ViewOrError(view View, err error) View
+
+	Escape(text string) HTML
+
+	Printf(text string, args ...interface{}) HTML
+
+	PrintfEscape(text string, args ...interface{}) HTML
+
+	A(url interface{}, content ...interface{}) Link
+
+	A_nofollow(url interface{}, content ...interface{}) Link
+
+	A_blank(url interface{}, content ...interface{}) *Link
+
+	A_blank_nofollow(url interface{}, content ...interface{}) *Link
+
+	STYLE(css string) HTML
+
+	StylesheetLink(url string) HTML
+
+	SCRIPT(javascript string) HTML
+
+	ScriptLink(url string) HTML
+
+	RSSLink(title string, url URL) View
+
+	IMG(url string, dimensions ...int)
+
+	SECTION(class string, content ...interface{}) View
+
+	DIV(class string, content ...interface{}) *Div
+
+	SPAN(class string, content ...interface{}) *Span
+
+	DivClearBoth() HTML
+
+	CANVAS(class string, width, height int) *Canvas
+
+	BR() HTML
+
+	HR() HTML
+
+	P(content ...interface{}) View
+
+	H1(content ...interface{}) View
+
+	H2(content ...interface{}) View
+
+	H3(content ...interface{}) View
+
+	H4(content ...interface{}) View
+
+	H5(content ...interface{}) View
+
+	H6(content ...interface{}) View
+
+	B(content ...interface{}) View
+
+	I(content ...interface{}) View
+
+	Q(content ...interface{}) View
+
+	DEL(content ...interface{}) View
+
+	EM(content ...interface{}) View
+
+	STRONG(content ...interface{}) View
+
+	DFN(content ...interface{}) View
+
+	CODE(content ...interface{}) View
+
+	PRE(content ...interface{}) View
+
+	ABBR(longTitle, abbreviation string) View
+
+	UL(items ...interface{}) *List
+
+	OL(items ...interface{}) *List
+
+	NewView(content interface{}) View
+
+	NewViews(contents ...interface{}) Views
+
+	WrapContents(contents ...interface{}) View
 */
 package view
--- view/shortcuts.go
+++ view/shortcuts.go
@@ -9,17 +9,12 @@ import (
 ///////////////////////////////////////////////////////////////////////////////
 // Shortcuts
 
-// ViewOrError returns a DynamicView that renders view if err is nil or
-// else renders the error.
-func ViewOrError(view View, err error) DynamicView {
-	return DynamicView(
-		func(ctx *Context) (View, error) {
-			if err != nil {
-				return nil, err
-			}
-			return view, nil
-		},
-	)
+// ViewOrError returns view if err is nil, or else an Error view for err.
+func ViewOrError(view View, err error) View {
+	if err != nil {
+		return NewError(err)
+	}
+	return view
 }
 
 // Escape HTML escapes a string.
@@ -37,42 +32,47 @@ func PrintfEscape(text string, args ...interface{}) HTML {
 	return Escape(fmt.Sprintf(text, args...))
 }
 
-// A creates a HTML link.
+// A creates <a href="url">content</a>
 func A(url interface{}, content ...interface{}) *Link {
 	return &Link{Model: NewLinkModel(url, content...)}
 }
 
-// A creates a HTML link.
+// A_nofollow creates <a href="url" rel="nofollow">content</a>
 func A_nofollow(url interface{}, content ...interface{}) *Link {
 	return &Link{Model: NewLinkModelRel(url, "nofollow", content...)}
 }
 
-// A_blank creates a HTML link with target="_blank"
+// A_blank creates <a href="url" target="_blank">content</a>
 func A_blank(url interface{}, content ...interface{}) *Link {
 	return &Link{NewWindow: true, Model: NewLinkModel(url, content...)}
 }
 
-// A_blank creates a HTML link with target="_blank" and rel="nofollow"
+// A_blank_nofollow creates <a href="url" target="_blank" rel="nofollow">content</a>
 func A_blank_nofollow(url interface{}, content ...interface{}) *Link {
 	return &Link{NewWindow: true, Model: NewLinkModelRel(url, "nofollow", content...)}
 }
 
+// STYLE creates <style>css</style>
 func STYLE(css string) HTML {
 	return Printf("<style>%s</style>", css)
 }
 
+// StylesheetLink creates <link rel='stylesheet' href='url'>
 func StylesheetLink(url string) HTML {
 	return Printf("<link rel='stylesheet' href='%s'>", url)
 }
 
-func SCRIPT(script string) HTML {
-	return Printf("<script>%s</script>", script)
+// SCRIPT creates <script>javascript</script>
+func SCRIPT(javascript string) HTML {
+	return Printf("<script>%s</script>", javascript)
 }
 
+// ScriptLink creates <script src='url'></script>
 func ScriptLink(url string) HTML {
 	return Printf("<script src='%s'></script>", url)
 }
 
+// RSSLink creates <link rel='alternate' type='application/rss+xml' title='title' href='url'>
 func RSSLink(title string, url URL) View {
 	return RenderView(
 		func(ctx *Context) error {
@@ -83,7 +83,7 @@ func RSSLink(title string, url URL) View {
 	)
 }
 
-// Img creates a HTML img element for an URL with optional width and height.
+// IMG creates a HTML img element for an URL with optional width and height.
 // The first int of dimensions is width, the second one height.
 func IMG(url string, dimensions ...int) *Image {
 	var width int
@@ -98,105 +98,133 @@ func IMG(url string, dimensions ...int) *Image {
 	return &Image{Src: url, Width: width, Height: height}
 }
 
+// SECTION creates <sections class="class">content</section>
 func SECTION(class string, content ...interface{}) View {
 	return &ShortTag{Tag: "section", Class: class, Content: WrapContents(content...)}
 }
 
-// Creates a Div object with a HTML class attribute and optional content.
+// DIV creates <div class="class">content</div>
 func DIV(class string, content ...interface{}) *Div {
 	return &Div{Class: class, Content: WrapContents(content...)}
 }
 
+// DIV creates <span class="class">content</span>
 func SPAN(class string, content ...interface{}) *Span {
 	return &Span{Class: class, Content: WrapContents(content...)}
 }
 
+// DivClearBoth creates <div style='clear:both'></div>
 func DivClearBoth() HTML {
 	return HTML("<div style='clear:both'></div>")
 }
 
+// CANVAS creates <canvas class="class" width="width" height="height"></canvas>
 func CANVAS(class string, width, height int) *Canvas {
 	return &Canvas{Class: class, Width: width, Height: height}
 }
 
+// BR creates <br/>
 func BR() HTML {
 	return HTML("<br/>")
 }
 
+// HR creates <hr/>
 func HR() HTML {
 	return HTML("<hr/>")
 }
 
+// P creates <p>content</p>
 func P(content ...interface{}) View {
 	return &ShortTag{Tag: "p", Content: WrapContents(content...)}
 }
 
+// H1 creates <h1>content</h1>
 func H1(content ...interface{}) View {
 	return &ShortTag{Tag: "h1", Content: WrapContents(content...)}
 }
 
+// H2 creates <h2>content</h2>
 func H2(content ...interface{}) View {
 	return &ShortTag{Tag: "h2", Content: WrapContents(content...)}
 }
 
+// H3 creates <h3>content</h3>
 func H3(content ...interface{}) View {
 	return &ShortTag{Tag: "h3", Content: WrapContents(content...)}
 }
 
+// H4 creates <h4>content</h4>
 func H4(content ...interface{}) View {
 	return &ShortTag{Tag: "h4", Content: WrapContents(content...)}
 }
 
+// H5 creates <h5>content</h5>
 func H5(content ...interface{}) View {
 	return &ShortTag{Tag: "h5", Content: WrapContents(content...)}
 }
 
+// H creates <h6>content</h6>
 func H6(content ...interface{}) View {
 	return &ShortTag{Tag: "h6", Content: WrapContents(content...)}
 }
 
+// B creates <b>content</b>
 func B(content ...interface{}) View {
 	return &ShortTag{Tag: "b", Content: WrapContents(content...)}
 }
 
+// I creates <i>content</i>
 func I(content ...interface{}) View {
 	return &ShortTag{Tag: "i", Content: WrapContents(content...)}
 }
 
+// Q creates <q>content</q>
 func Q(content ...interface{}) View {
 	return &ShortTag{Tag: "q", Content: WrapContents(content...)}
 }
 
+// DEL creates <del>content</del>
 func DEL(content ...interface{}) View {
 	return &ShortTag{Tag: "del", Content: WrapContents(content...)}
 }
 
+// EM creates <em>content</em>
 func EM(content ...interface{}) View {
 	return &ShortTag{Tag: "em", Content: WrapContents(content...)}
 }
 
+// STRONG creates <strong>content</strong>
 func STRONG(content ...interface{}) View {
 	return &ShortTag{Tag: "strong", Content: WrapContents(content...)}
 }
 
+// DFN creates <dfn>content</dfn>
 func DFN(content ...interface{}) View {
 	return &ShortTag{Tag: "dfn", Content: WrapContents(content...)}
 }
 
+// CODE creates <code>content</code>
 func CODE(content ...interface{}) View {
 	return &ShortTag{Tag: "code", Content: WrapContents(content...)}
 }
 
+// PRE creates <pre>content</pre>
 func PRE(content ...interface{}) View {
 	return &ShortTag{Tag: "pre", Content: WrapContents(content...)}
 }
 
+// ABBR creates <abbr title="longTitle">abbreviation</abbr>
 func ABBR(longTitle, abbreviation string) View {
 	return &ShortTag{Tag: "abbr", Attribs: map[string]string{"title": longTitle}, Content: Escape(abbreviation)}
 }
 
-// Ul is a shortcut to create an unordered list by wrapping items as HTML views.
+// UL is a shortcut to create an unordered list by wrapping items as HTML views.
 // NewView will be called for every passed item.
+//
+// Example:
+//   UL("red", "green", "blue")
+//   UL(A(url1, "First Link"), A(url2, "Second Link"))
+//
 func UL(items ...interface{}) *List {
 	model := make(ViewsListModel, len(items))
 	for i, item := range items {
@@ -205,15 +233,20 @@ func UL(items ...interface{}) *List {
 	return &List{Model: model}
 }
 
-// Ul is a shortcut to create an ordered list by wrapping items as HTML views.
+// OL is a shortcut to create an ordered list by wrapping items as HTML views.
 // NewView will be called for every passed item.
+//
+// Example:
+//   OL("red", "green", "blue")
+//   OL(A(url1, "First Link"), A(url2, "Second Link"))
+//
 func OL(items ...interface{}) *List {
 	list := UL(items...)
 	list.Ordered = true
 	return list
 }
 
-// Encapsulates content as View.
+// NewView encapsulates content as View.
 // Strings or fmt.Stringer implementations will be HTML escaped.
 // View implementations will be passed through. 
 func NewView(content interface{}) View {
@@ -233,7 +266,8 @@ func NewView(content interface{}) View {
 	return Escape(v.String())
 }
 
-// Encapsulates multiple content arguments as views by calling NewView() for every one of them.
+// NewViews encapsulates multiple content arguments as views by calling NewView()
+// for every one of them.
 func NewViews(contents ...interface{}) Views {
 	count := len(contents)
 	if count == 0 {
@@ -246,7 +280,8 @@ func NewViews(contents ...interface{}) Views {
 	return views
 }
 
-// Encapsulates multiple content arguments as View by calling NewView() for every one of them.
+// WrapContents encapsulates multiple content arguments as View by calling NewView()
+// for every one of them.
 // It is more efficient for one view because the view is passed through instead of wrapped
 // with a Views slice like NewViews does.
 func WrapContents(contents ...interface{}) View {
